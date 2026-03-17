1ca471997a82767695a1c41f9dbc6dfc569a860c
avnue <avnue@github.com>
2026-03-18 04:08:38 +0530

    Fix brace syntax error in GUI.h causing build failure

--- CS2_External/Features/GUI.h
+++ CS2_External/Features/GUI.h
@@ -79,6 +79,7 @@ namespace GUI
 	void LoadImages()
 	{
 		if (LUL_Logo == NULL)
+		{
 			Gui.LoadTextureFromMemory(Images::LUL_Logo, sizeof Images::LUL_Logo, &LUL_Logo, &LogoW, &LogoH);
 			Gui.LoadTextureFromMemory(Images::VisualButton, sizeof Images::VisualButton, &MenuButton1, &buttonW, &buttonH);
 			Gui.LoadTextureFromMemory(Images::AimbotButton, sizeof Images::AimbotButton, &MenuButton2, &buttonW, &buttonH);
